18be1e1e603a2ddf316b7b618a6d286ee7353944
turtle-insect <turtle.insect@gmail.com>
2022-07-31 19:51:06 +0900

    support character's class param

--- Xenoblade3/Character.cs
+++ Xenoblade3/Character.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,16 @@ namespace Xenoblade3
 	{
 		private readonly uint mAddress;
 
+		public ObservableCollection<Class> Classes { get; private set; } = new ObservableCollection<Class>();
+
 		public Character(uint address)
 		{
 			mAddress = address;
+
+			for(uint i = 0; i < 23; i++)
+			{
+				Classes.Add(new Class(mAddress + 0x14 + i * 68));
+			}
 		}
 
 		public uint Lv
---
+++ Xenoblade3/Class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xenoblade3
+{
+	internal class Class
+	{
+		private readonly uint mAddress;
+
+		public Class(uint address)
+		{
+			mAddress = address;
+		}
+
+		public uint Lv
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 6, 1);
+			set => Util.WriteNumber(mAddress + 6, 1, 0, 16, value);
+		}
+
+		public uint Exp
+		{
+			get => SaveData.Instance().ReadNumber(mAddress, 4);
+			set => Util.WriteNumber(mAddress, 4, 0, 99999999, value);
+		}
+	}
+}
